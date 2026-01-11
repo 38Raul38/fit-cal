4beb0567bb1ce22f2c003f89cd79cf9de89943fb
38Raul38 <raulseidov2006@gmail.com>
2026-01-11 18:42:38 +0400

    Project Build

---
+++ src/FitCal.Application/MappingConfigurations/MappingProfile.cs
@@ -0,0 +1,6 @@
+﻿namespace FitCal.Application.MappingConfigurations;
+
+public class MappingProfile
+{
+    
+}
---
+++ src/FitCal.Persistence/Migrations/20260110083650_First.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace FitCal.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class First : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Foods",
+                columns: table => new
+                {
+                    FoodId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    ServingSize = table.Column<double>(type: "double precision", nullable: false),
+                    ServingUnit = table.Column<string>(type: "text", nullable: false),
+                    Calories = table.Column<double>(type: "double precision", nullable: false),
+                    Protein = table.Column<double>(type: "double precision", nullable: false),
+                    Carbs = table.Column<double>(type: "double precision", nullable: false),
+                    Fats = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Foods", x => x.FoodId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserInformations",
+                columns: table => new
+                {
+                    UserInformationId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    BirthDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Gender = table.Column<string>(type: "text", nullable: false),
+                    Height = table.Column<double>(type: "double precision", nullable: false),
+                    Weight = table.Column<double>(type: "double precision", nullable: false),
+                    WeightGoal = table.Column<double>(type: "double precision", nullable: false),
+                    ActivityLevel = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserInformations", x => x.UserInformationId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Recipes",
+                columns: table => new
+                {
+                    RecipeId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    RecipeName = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    ServingSize = table.Column<double>(type: "double precision", nullable: false),
+                    ServingUnit = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    UserInformationId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Recipes", x => x.RecipeId);
+                    table.ForeignKey(
+                        name: "FK_Recipes_UserInformations_UserInformationId",
+                        column: x => x.UserInformationId,
+                        principalTable: "UserInformations",
+                        principalColumn: "UserInformationId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RecipeIngredients",
+                columns: table => new
+                {
+                    RecipeId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    RecipeIngredientId = table.Column<int>(type: "integer", nullable: false),
+                    RecipeId1 = table.Column<int>(type: "integer", nullable: false),
+                    FoodId = table.Column<int>(type: "integer", nullable: false),
+                    Quantity = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RecipeIngredients", x => x.RecipeId);
+                    table.ForeignKey(
+                        name: "FK_RecipeIngredients_Foods_FoodId",
+                        column: x => x.FoodId,
+                        principalTable: "Foods",
+                        principalColumn: "FoodId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RecipeIngredients_Recipes_RecipeId1",
+                        column: x => x.RecipeId1,
+                        principalTable: "Recipes",
+                        principalColumn: "RecipeId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserHistories",
+                columns: table => new
+                {
+                    UserInformationId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserHistoryId = table.Column<int>(type: "integer", nullable: false),
+                    JournalDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    FoodId = table.Column<int>(type: "integer", nullable: false),
+                    UserInformationId1 = table.Column<int>(type: "integer", nullable: false),
+                    RecipeId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserHistories", x => x.UserInformationId);
+                    table.ForeignKey(
+                        name: "FK_UserHistories_Foods_FoodId",
+                        column: x => x.FoodId,
+                        principalTable: "Foods",
+                        principalColumn: "FoodId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserHistories_Recipes_RecipeId",
+                        column: x => x.RecipeId,
+                        principalTable: "Recipes",
+                        principalColumn: "RecipeId");
+                    table.ForeignKey(
+                        name: "FK_UserHistories_UserInformations_UserInformationId1",
+                        column: x => x.UserInformationId1,
+                        principalTable: "UserInformations",
+                        principalColumn: "UserInformationId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reports",
+                columns: table => new
+                {
+                    ReportId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ReportName = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    ReportDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UserHistoryId = table.Column<int>(type: "integer", nullable: true),
+                    UserInformationId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reports", x => x.ReportId);
+                    table.ForeignKey(
+                        name: "FK_Reports_UserHistories_UserHistoryId",
+                        column: x => x.UserHistoryId,
+                        principalTable: "UserHistories",
+                        principalColumn: "UserInformationId");
+                    table.ForeignKey(
+                        name: "FK_Reports_UserInformations_UserInformationId",
+                        column: x => x.UserInformationId,
+                        principalTable: "UserInformations",
+                        principalColumn: "UserInformationId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecipeIngredients_FoodId",
+                table: "RecipeIngredients",
+                column: "FoodId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecipeIngredients_RecipeId1",
+                table: "RecipeIngredients",
+                column: "RecipeId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recipes_UserInformationId",
+                table: "Recipes",
+                column: "UserInformationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reports_UserHistoryId",
+                table: "Reports",
+                column: "UserHistoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reports_UserInformationId",
+                table: "Reports",
+                column: "UserInformationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserHistories_FoodId",
+                table: "UserHistories",
+                column: "FoodId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserHistories_RecipeId",
+                table: "UserHistories",
+                column: "RecipeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserHistories_UserInformationId1",
+                table: "UserHistories",
+                column: "UserInformationId1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RecipeIngredients");
+
+            migrationBuilder.DropTable(
+                name: "Reports");
+
+            migrationBuilder.DropTable(
+                name: "UserHistories");
+
+            migrationBuilder.DropTable(
+                name: "Foods");
+
+            migrationBuilder.DropTable(
+                name: "Recipes");
+
+            migrationBuilder.DropTable(
+                name: "UserInformations");
+        }
+    }
+}
